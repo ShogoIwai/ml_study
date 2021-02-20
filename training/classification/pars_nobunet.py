833ebb616818d54f83e9bd2a06418462e250d987
iwai.shogo <iwai.shogo@megachips.co.jp>
2021-02-20 22:27:49 +0900

    updated image dump script

--- training/classification/pars_nobunet.py
+++ training/classification/pars_nobunet.py
@@ -6,11 +6,11 @@ import sys
 sys.path.append(os.path.abspath('../../'))
 from common.arydmp import arydmp as ad
 
-def read_hdf5(rpath, wpath):
-    if not os.path.isdir(f'./{weights_dirname}'):
-        os.mkdir(f'./{weights_dirname}')
+def read_hdf5(rfile, wpath):
+    if not os.path.isdir(f'{wpath}'):
+        os.mkdir(f'{wpath}')
 
-    with h5py.File(rpath, 'r') as f: # open file
+    with h5py.File(f'{rfile}', 'r') as f: # open file
         keys = []
         f.visit(keys.append) # append all keys to list
         for key in keys:
@@ -20,7 +20,7 @@ def read_hdf5(rpath, wpath):
                 write_file = re.sub(r'\/', '_', write_file)
                 write_file = re.sub(':0$', '', write_file)
                 array_name = write_file
-                write_file = re.sub('^', f'./{wpath}/', write_file)
+                write_file = re.sub('^', f'{wpath}/', write_file)
                 write_file = re.sub('$', '.h', write_file)
                 try:
                     if re.search('optimizer_weights', write_file): raise Exception
@@ -29,6 +29,6 @@ def read_hdf5(rpath, wpath):
                     pass
 
 if __name__ == '__main__':
-    model_filename = 'cats_dogs_giraffes_elephants_lions_classification.h5'
-    weights_dirname = 'weights'
+    model_filename = './cats_dogs_giraffes_elephants_lions_classification.h5'
+    weights_dirname = './weights'
     read_hdf5(model_filename, weights_dirname)
