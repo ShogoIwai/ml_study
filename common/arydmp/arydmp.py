ad51759f7c51219727bc17bc98de4b870decf05d
iwai.shogo <iwai.shogo@megachips.co.jp>
2021-02-22 00:16:17 +0900

    modified arydmp

--- common/arydmp/arydmp.py
+++ common/arydmp/arydmp.py
@@ -5,5 +5,11 @@ def array_dump(array, fname, aname):
     disp = f'generating {fname} ..., shape={array.shape}, dtype={array.dtype}'
     print (disp)
     array = array.flatten()
-    header = '// ' + disp + '\n' + f'float {aname}[] = ' + '{'
-    np.savetxt(fname, array, header=header, footer='};', delimiter=',', newline=',\n', comments='')
+    header = '// ' + disp + '\n' + f'float {aname}[] = ' + '{' + '\n'
+    ofs = open(fname, mode='w')
+    ofs.write(header)
+    for i in range(array.size):
+        ofs.write(f'{array[i]}')
+        if (i != (array.size-1)): ofs.write(',\n')
+    ofs.write('};')
+    ofs.close()
